a3a6626a6a73b7e67d3f3299cbe7cb10721ad0a5
CHeitzenrater <c@Auchentoshan.local>
2017-05-04 15:38:34 +0100

    Filled out model; added costs
    Further filled out the model and function parameters.  Added cost
    functions.

--- models/COSECMO.R
+++ models/COSECMO.R
@@ -3,13 +3,14 @@
 
 # This code can be used to generate the data in the COSECMO_Data.R file. 
 
-
+###
+###
 #### BASE COCOMO
-
-# The 1.18 are multiplier factors to account for a 6% inception and 12% transition; typical COCOMO values
-EffortDevel = val_EffortEC * 1.18
+###
+###
 
 ###
+# EffortEC()
 # A				Effort coefficient; nominally 2.94, can be calibrated to a specific organisation
 # size			??
 # val_E			The value for E, as defined by the E() function
@@ -20,6 +21,7 @@ EffortEC <- function(A=2.94, size=1, val_E, val_EM=1){
 }
 
 ###
+# E()
 # B 				Scaling exponent; nominally 0.91, can be calibrated to a specific organisation
 # val_ScaleFactors	The summation of the scale factor values
 #
@@ -27,16 +29,44 @@ E <- function( B=0.91, val_ScaleFactors=1 ){
 	return( B + 0.01 * val_ScaleFactors )
 }
 
+###
+# val_Effort 		Value of the effort, in man-months
+# val_LaborRate		Value of the labor rate, units undertermined (pounds, dollars)
+#
 Cost <- function( val_Effort, val_LaborRate ){
 	return( val_Effort * val_LaborRate ) 
 }
 
 
-####### COSECMO Extensions
 
-EffortAssuredDevel = val_EffortAssuredEC * 1.18
 
-EffortAssuredEC = val_EffortEC + val_EffortIntAssure
+
+
+
+
+###
+###
+#### COSECMO Extensions
+###
+###
+
+###
+# EffortAssuredDevel()
+# From the paper: valEffortAssuredDevel = val_EffortAssuredEC * 1.18.  The 1.18 are multiplier factors to account for a 6% inception and 12% transition; typical COCOMO values
+#
+EffortAssuredDevel <- function( val_EffAssuredEC, val_effAssureECMult = 1.18 ){
+	return( val_EffAssuredEC * val_effAssureECMult ) 
+}
+
+
+#valEffortAssuredEC = val_EffortEC + val_EffortIntAssure
+###
+# val_EffortEC			Effort in person-months
+# val_EffortIntAssure	The effort spent on internal assurance	
+#
+EffortAssureEC <- function( val_EffortEC, val_EffortIntAssure ){
+	return( val_EffortEC + val_EffortIntAssure )
+}
 
 ###
 # val_EffortEC			Effort in person-months
@@ -60,16 +90,30 @@ EffortPercentAL <- function( effort3, SECU=2.5, EAL ){
 	return( ret )
 }
 
+
+
+######## Calculate overall terms
+
+CalculateCostAssureDevel <- function( effortEC, effort3, SECU=2.5, EAL, laborRate ){
+	eff_ia = EffortIntAssure( effortEC, EffortPercentAL( effort3, SECU, EAL) ) 
+	eff_aEC = EffortAssureEC( effortEC, eff_ia )
+	eff_aDevel = EffortAssuredDevel( eff_aEC )
+	return( Cost( eff_aDevel, laborRate )  )	
+}
+
+CalculateCostIntAssure <- function( effortEC, effort3, SECU=2.5, EAL, laborRate ){
+	eff_ia = EffortIntAssure( effortEC, EffortPercentAL( effort3, SECU, EAL) ) 
+	return( Cost( eff_ia, laborRate )  )
+}
+
+CalculateCostIntAssure_PM <- function( effortEC, effort3, SECU=2.5, EAL ){
+	eff_ia = EffortIntAssure( effortEC, EffortPercentAL( effort3, SECU, EAL) ) 
+	return( eff_ia )
+}
+
 TotalAssuredCost <- function( val_CostAssureDevel, val_CostIndAssure ){
 	return( val_CostAssureDevel + val_CostIndAssure ) 
 }
-
-
-
-
-
-
-
 
 
 
